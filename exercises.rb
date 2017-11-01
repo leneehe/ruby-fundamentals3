da641cf1bc97a87fffb6496e7c9e26d1724594a4
Lena <fatmeow@gmail.com>
2017-11-01 16:29:57 -0400

    Complete exercise 0

---
+++ exercises.rb
@@ -0,0 +1,23 @@
+#Exercise 0
+#Arrays
+fav_colours = ["yellow", "grey", "babyblue", "lilac", "pink"]
+ages = ["30", "27", "25", "28", "42"]
+heads = [true, true, false, true, false]
+fav_artists = ["Leonardo Da Vinci", "Salvador Dali", "Joan Miro"]
+fav_colours2 = [:yellow, :grey, :babyblue, :lilac, :pink]
+
+#Hashes
+words = {
+  serendipity:"Occurence and development of events by change in a happy way.",
+  coulrophobia:"Extreme or irrational fear of clowns.",
+  ophthalmitis:"Patron Goddess of eyesight. Surname of Athena in Sparta."
+}
+fav_movies = {
+  "Shawshank Redemption" => 1994,
+  "Watchmen" => 2009,
+  "Prometheus" => 2012,
+  "Pan's Labyrinth" => 2006
+}
+cities_population = { toronto: 2809000, new_york: 8538000, tokyo: 9273000
+}
+friends = {"Edward" => 29, "Kevin" => 27, "Brook" => 25, "Meng" => 25, "Richard" => 37}
