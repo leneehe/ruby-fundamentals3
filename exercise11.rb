9d37a014703454396681bc1689b7f720276b5b10
Lena <fatmeow@gmail.com>
2017-11-02 02:36:55 -0400

    Complete exercise 11

---
+++ exercise11.rb
@@ -0,0 +1,17 @@
+
+puts "How many pizzas do you want to order?"
+pizza_numbers = gets.to_i
+
+pizzas = []
+pizzas = Array.new(pizza_numbers)
+
+def topping_order(pizza)
+
+  pizza.each_index { |n|
+    puts "How many toppings for pizza #{n + 1} ?"
+    toppings = gets.to_i
+    puts "You have ordered a pizza with #{toppings} toppings."
+  }
+end
+
+topping_order(pizzas)
